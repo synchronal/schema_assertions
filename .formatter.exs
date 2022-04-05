063d8175674f0fc10b878963bb556d1415164c1a
Eric Saxby <sax@livinginthepast.org>
2022-04-05 15:02:37 -0700

    Formatter exports local_without_parens
    Signed-off-by: Erik Hanson <erik@eahanson.com>
    Co-authored-by: Erik Hanson <erik@eahanson.com>

--- .formatter.exs
+++ .formatter.exs
@@ -3,5 +3,10 @@
   import_deps: [:ecto],
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
   line_length: 120,
-  subdirectories: ["priv/*/migrations"]
+  subdirectories: ["priv/*/migrations"],
+  export: [
+    locals_without_parens: [
+      assert_schema: 3
+    ]
+  ]
 ]
